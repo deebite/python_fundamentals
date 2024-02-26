de740ea6c3432883ca39febd02f6b56d7152bf84
Amardeep <amardeeplokare@wfs.co.za>
2024-02-26 14:12:20 +0530

    Added: tuple and List Data type added

---
+++ ClassConcept.py
@@ -0,0 +1,12 @@
+class MyCalss():
+
+    def myfunction(self, name):
+        print(f"Hi all myFunc printed {name}")
+
+
+c1 = MyCalss()
+c1.myfunction("Amar deep")
+
+f = str(open("DictDataType.py"))
+
+print(bytearray(f,"utf"))
